4efb8c8e82ffea6168c6bda471bfec1a52d11046
Quarcoo-arc <53879894+Quarcoo-arc@users.noreply.github.com>
2021-05-17 16:43:32 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//vanilla javascrpt
+//var React = require("react");
+//var ReactDOM = require("react-dom");
+
+//Next Gen javascript code
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a new paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
